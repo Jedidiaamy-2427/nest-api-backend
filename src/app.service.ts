e4792607a46d3722793da25ce9c25ea75f22d00c
Jedidiaamy-2427 <jedidiaamyandrianjafi@gmail.com>
2025-01-29 17:00:31 +0300

    Welcome api page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<html><head><title>Welcome to my API</title></head><body><h1>Welcome to my API</h1></body></html>';
   }
 }
