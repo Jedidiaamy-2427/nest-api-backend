e574f43ecf685e8bbbfa14c8a115dbe848cb79a3
Jedidiaamy-2427 <jedidiaamyandrianjafi@gmail.com>
2025-05-23 15:11:52 +0300

    Rectification auth modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -8,14 +8,19 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { UserService } from '../user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/modules/user/entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60m' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
     }),
     TypeOrmModule.forFeature([User]),
   ],
